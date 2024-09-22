0f953aea6e86f500f82268289d053396d29fcc8d
mtrueffer <mtrueffer@outlook.com>
2024-09-21 22:44:00 -0400

    Started work on the screen objects

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,8 @@ def main():
                 return
         pygame.Surface.fill(screen, color="black")
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
